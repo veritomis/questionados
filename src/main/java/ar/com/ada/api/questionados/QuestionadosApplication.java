ec982c1a1d4f4e20cb91c18a51445fc51774c1cd
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-06-30 19:11:59 -0300

    primer commit

---
+++ src/main/java/ar/com/ada/api/questionados/QuestionadosApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.questionados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionadosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionadosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ada/api/questionados/QuestionadosApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ada.api.questionados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
