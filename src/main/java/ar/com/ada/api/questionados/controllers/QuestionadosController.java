a76fa2bad0c154b59e585477ccef35222ef7dae8
BrendaMonteagudo1 <brendamonteagudo@gmail.com>
2021-07-06 16:47:57 -0300

    se agregar el reqquest RespuestaAVerificar

--- src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
+++ src/main/java/ar/com/ada/api/questionados/controllers/QuestionadosController.java
@@ -1,13 +1,16 @@
 package ar.com.ada.api.questionados.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-
-import ar.com.ada.api.questionados.entities.Pregunta;
-import ar.com.ada.api.questionados.services.QuestionadosService;
-
 import org.springframework.web.bind.annotation.*;
+
+import ar.com.ada.api.questionados.services.CategoriaService;
+import ar.com.ada.api.questionados.services.QuestionadosService;
+import ar.com.ada.api.questionados.entities.Categoria;
+import ar.com.ada.api.questionados.entities.Pregunta;
+import ar.com.ada.api.questionados.models.response.GenericResponse;
 
 @RestController
 public class QuestionadosController {
---
+++ src/main/java/ar/com/ada/api/questionados/models/request/RespuestaAVerificar.java
@@ -0,0 +1,7 @@
+package ar.com.ada.api.questionados.models.request;
+
+public class RespuestaAVerificar {
+
+    public Integer respuestaId;
+    public Integer preguntaId;
+}
